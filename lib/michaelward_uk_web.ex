ec821d95de3b0eca1d2fcde02d8e610ecd79523e
Michael Ward <michael@scripthungry.com>
2022-11-06 09:52:14 +0000

    Upgrades phoenix_live_view to 0.18

--- lib/michaelward_uk_web.ex
+++ lib/michaelward_uk_web.ex
@@ -89,8 +89,8 @@ defmodule MichaelwardUkWeb do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
-      # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
-      import Phoenix.LiveView.Helpers
+      # Import LiveView and .heex helpers
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
