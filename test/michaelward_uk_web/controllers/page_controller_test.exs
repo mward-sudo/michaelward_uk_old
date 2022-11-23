1c2ce8f2157c0aa565787d5023337679f9673023
Michael Ward <michael@scripthungry.com>
2022-11-23 17:07:34 +0000

    Update test

--- test/michaelward_uk_web/controllers/page_controller_test.exs
+++ test/michaelward_uk_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MichaelwardUkWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Michael Ward"
   end
 end
